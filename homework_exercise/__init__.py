82bee0f42209e4a7510cbd793304859c5a72efc8
nataliarobak <94309659+nataliarobak@users.noreply.github.com>
2022-01-23 17:24:40 +0100

    Add files via upload

---
+++ homework_exercise/__init__.py
@@ -0,0 +1,3 @@
+from homework_exercise._calculation import some_calculation_function, some_calculation_function2
+
+__all__ = ("some_calculation_function", "some_calculation_function2")
---
+++ homework_exercise/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+  name="homework_exercise",
+  version="1.0.0",
+  scripts=["some_calculation_function"]
+)
